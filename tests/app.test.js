79cb440207f8274b540b948f9d5e07e652485689
BertCasale <bthebest55@gmail.com>
2023-04-04 21:30:59 -0400

    lab complete

---
+++ tests/app.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+const app = require("../app.js");
+
+it("Testing to see if supertest works", () => {
+  expect(1).toBe(1)
+})
+
+describe("Test the root path", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+  })
+})
+
+describe("Test the gashbell path", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/gashbell")
+      .then(response => {
+        expect(response.text).toBe(`I'll become a kind king`)
+      })
+  })
+})
