74fe1ea1133aa8be50210601aa34503c769affba
USERNAME <EMAIL>
2023-04-04 21:39:25 -0400

    lab-intro-to-express-response -Done

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require ("../app.js")
+
+// DEPENDENCIES
+const app = require('./app.js')
+
+// CONFIGURATION
+const PORT = 3005
+
+// LISTEN
+app.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`)
+})
